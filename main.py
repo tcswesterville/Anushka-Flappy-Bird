d42e31d613e3642126dcb8c177d340fe52732fe4
Anushkaisqueen <anushkathequeen777@gmail.com>
2024-11-09 10:32:28 -0500

    Pipe Spawning
    This adds basic pipe spawning, and dying from it.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
 pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("Sounds/YPIL.mp3")
+pygame.mixer.music.play(-1)
 WINDOWS_HEIGHT=800
 WINDOWS_WIDTH=800
 
@@ -15,18 +18,39 @@ Backround=pygame.image.load(Backroundimage)
 
 Pipeimage= "images/pipe 4 CF.png"
 Pipe = pygame.image.load(Pipeimage)
-Playerimage= "images/PP.jpg"
+Playerimage= "images/FlappyBlue.png"
 
-Player = pygame.image.load(Playerimage)
+PipeList = []
+
+Player = pygame.image.load(Playerimage)  
 Player = pygame.transform.scale(Player, (100, 100))
+Pipeupsidedown = pygame.transform.flip(Pipe,False, True)
 
 Playerposition = Player.get_rect()
+Pipeposition = Pipe.get_rect()
 Playerposition.y = WINDOWS_HEIGHT/2
 Playerposition.x = WINDOWS_WIDTH/2
+Pipeposition.x = WINDOWS_HEIGHT/1   
+Pipeposition.y = WINDOWS_WIDTH/1.6    
+PipeList.append(Pipeposition)
+
+gameoverfont=pygame.font.Font("DancingScript-Regular.ttf", 40)
+gameovertext = gameoverfont.render("Game Over! ;)", True ,(0,0,0) , (255,255,255))
+
+
 while running:
+    for sigmapipe in PipeList: 
+        sigmapipe.x-=1
+    pygame.time.Clock().tick(60)
     screen.blit(Backround, Backround.get_rect())
     # screen blit takes (image, (x, y))
-    screen.blit(Pipe,(0,4))
+    screen.blit(Pipeupsidedown,(Pipeposition))
+
+ 
+# Colision Code
+    for EachPipe in PipeList:
+       if Playerposition.colliderect(EachPipe): 
+            running = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,8 +59,8 @@ while running:
         if pressed[pygame.K_ESCAPE]:
             pygame.quit()
         if pressed[pygame.K_SPACE]:
-            Playerposition.y -= 45
-    Playerposition.y += 3
+            Playerposition.y -= 80
+    Playerposition.y += 5
     screen.blit(Player, Playerposition)
     pygame.display.update()
-
+     
