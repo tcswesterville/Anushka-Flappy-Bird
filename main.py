245bdb1b1d687fb732f892879e6e479bcb53c8ba
Anushkaisqueen <anushkathequeen777@gmail.com>
2024-12-21 11:54:56 -0500

    add score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 pygame.init()
 pygame.mixer.init()
@@ -16,41 +17,82 @@ running=True
 Backroundimage= "images/dont worry abt it.jpg"
 Backround=pygame.image.load(Backroundimage)
 
-Pipeimage= "images/pipe 4 CF.png"
-Pipe = pygame.image.load(Pipeimage)
+score = 0  
+
 Playerimage= "images/FlappyBlue.png"
 
 PipeList = []
 
 Player = pygame.image.load(Playerimage)  
 Player = pygame.transform.scale(Player, (100, 100))
-Pipeupsidedown = pygame.transform.flip(Pipe,False, True)
-
 Playerposition = Player.get_rect()
-Pipeposition = Pipe.get_rect()
 Playerposition.y = WINDOWS_HEIGHT/2
 Playerposition.x = WINDOWS_WIDTH/2
-Pipeposition.x = WINDOWS_HEIGHT/1   
-Pipeposition.y = WINDOWS_WIDTH/1.6    
-PipeList.append(Pipeposition)
+
 
 gameoverfont=pygame.font.Font("DancingScript-Regular.ttf", 40)
 gameovertext = gameoverfont.render("Game Over! ;)", True ,(0,0,0) , (255,255,255))
 
+class Pipe:
+    def __init__(self):
+        gap = 450  
+        random_pipe_length = randint(100, WINDOWS_HEIGHT- gap-100)
+        pipe_image = pygame.image.load("images/pipe 4 CF.png")
+        scale_pipe_image = pygame.transform.scale(pipe_image,(50, random_pipe_length))
 
-while running:
-    for sigmapipe in PipeList: 
-        sigmapipe.x-=1
+        self.top_pipe_image = pygame.transform.flip(scale_pipe_image, False, True)
+
+        bottom_pipe_height = WINDOWS_HEIGHT - random_pipe_length - gap
+        self.bottom_pipe_image = scale_pipe_image
+
+        self.top_pipe = self.top_pipe_image.get_rect()
+        self.bottom_pipe = self.bottom_pipe_image.get_rect()
+
+        self.top_pipe.y = 0
+        self.bottom_pipe.y = WINDOWS_HEIGHT - bottom_pipe_height
+
+        self.top_pipe.x = WINDOWS_WIDTH
+        self.bottom_pipe.x = WINDOWS_WIDTH
+
+        self.made_next_pipe = False
+        self.add_score = False
+        PipeList.append(self)
+
+
+        
+       
+
+    def move_pipe(self):
+        self.bottom_pipe.x -= 3
+        self.top_pipe.x -= 3
+
+        if Playerposition.colliderect(self.top_pipe) or Playerposition.colliderect(self.bottom_pipe):
+            pygame.quit()
+
+        if self.bottom_pipe.x < WINDOWS_WIDTH / 2 and not self.made_next_pipe:
+            new_pipe = Pipe()
+            PipeList.append(new_pipe)
+            self.made_next_pipe = True
+        if self.bottom_pipe.x < 0:
+            PipeList.remove(self)
+        
+        if Playerposition.x > self.bottom_pipe.x and not self.add_score:
+            global score 
+            score += 1
+            print (score)
+
+            self.add_score = True  
+
+
+
+        
+
+first_pipe = Pipe()
+
+while running:  
     pygame.time.Clock().tick(60)
     screen.blit(Backround, Backround.get_rect())
     # screen blit takes (image, (x, y))
-    screen.blit(Pipeupsidedown,(Pipeposition))
-
- 
-# Colision Code
-    for EachPipe in PipeList:
-       if Playerposition.colliderect(EachPipe): 
-            running = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,5 +104,10 @@ while running:
             Playerposition.y -= 80
     Playerposition.y += 5
     screen.blit(Player, Playerposition)
+
+    for pipe in PipeList:
+        screen.blit(pipe.top_pipe_image, pipe.top_pipe)
+        screen.blit(pipe.bottom_pipe_image, pipe.bottom_pipe)
+        pipe.move_pipe()
     pygame.display.update()
      
